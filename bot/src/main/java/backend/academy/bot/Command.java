744adfa865b011e769178697381207a10894e0a6
Andrey Vorotnikov <vorotnikovandrej46@gmail.com>
2025-03-03 22:20:53 +0300

    fix: linters

--- bot/src/main/java/backend/academy/bot/Command.java
+++ bot/src/main/java/backend/academy/bot/Command.java
@@ -2,5 +2,5 @@ package backend.academy.bot;
 
 @FunctionalInterface
 public interface Command {
-    public void execute(Long chatId, String params);
+    void execute(Long chatId, String params);
 }
